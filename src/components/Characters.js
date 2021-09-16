10d1a02e217fccda28c4bd72d707d930e9855f42
RobertMaina <robertmaina254@gmail.com>
2021-09-16 13:47:09 +0300

    add key prop

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -5,7 +5,7 @@ const Characters = ({ charactersItems, isLoading }) => {
     return isLoading ? (<h1>Loading...</h1>) : (
         <div className= "cards">
             {charactersItems.map((item) => (
-                <h1>
+                <h1 key={item.char_id}>
                     {item.name}
                 </h1>
             ))}
