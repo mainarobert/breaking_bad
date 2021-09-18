8140daddd463f33d2916c74e4638137923ed03a4
RobertMaina <robertmaina254@gmail.com>
2021-09-18 19:40:28 +0300

    add local state to search component and refactor app component

--- src/App.js
+++ src/App.js
@@ -3,27 +3,30 @@ import axios from 'axios'
 import './App.css'
 import Header from './components/Header'
 import Characters from './components/Characters'
+import Search from './components/Search'
 
 
 const App = () => {
 
     const [charactersItems, setCharacters]= useState([])
     const [isLoading, setIsLoading]= useState(true)
+    const [query, setQuery]= useState('')
     
     useEffect(() => {
         const fetchItems = async () => {
-            const result = await axios (`https://www.breakingbadapi.com/api/characters`)
+            const result = await axios (`https://www.breakingbadapi.com/api/characters?name=${query}`)
             console.log(result.data)
             setCharacters(result.data)
             setIsLoading(false)
         }
         fetchItems()
 
-    }, [])
+    }, [query])
 
     return (
         <div className= "container">
             <Header />
+            <Search getQuery= {(q) => setQuery(q)}/>
             <Characters isLoading = {isLoading} charactersItems = {charactersItems}/>
         </div>
     )
